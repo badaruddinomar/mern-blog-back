923985906501d72c612661efdefa4c0d6e76d617
Badar <86910185+badaruddinomar@users.noreply.github.com>
2024-01-04 12:07:17 +0600

    cors middlerare added

--- index.js
+++ index.js
@@ -19,14 +19,24 @@ const { frontendUrl } = require("./helper.js");
 const app = express();
 const salt = bcrypt.genSaltSync(10);
 
-const corsOptions = {
-  origin: frontendUrl,
-  credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  // optionsSuccessStatus: 204,
-  // allowedHeaders: "Content-Type,Authorization",
-};
-app.use(cors(corsOptions));
+// Set middleware of CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", frontendUrl);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
